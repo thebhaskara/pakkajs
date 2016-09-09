677ac993f0a35516783f04fec63cf09afa56e763
Lalu Lavanya Busam <lalu@Lalus-MacBook-Air.local>
2016-09-09 23:56:06 +0530

    deleting crap

--- app/controllers/header.js
+++
@@ -1,9 +0,0 @@
-define([
-    'app'
-], function(app) {
-    app.controller('HeaderCtrl', ['$scope', 'MyService', function($scope, MyService) {
-		$scope.search = "he he he";
-        $scope.mm = MyService;
-    }]);
-    return app;
-})
