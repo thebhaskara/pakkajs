4a77d8cd6c3975cb2ccaaba16bf1ba0844971de3
Lalu Lavanya Busam <lalu@Lalus-MacBook-Air.local>
2016-09-11 18:18:24 +0530

    some changes

--- src/BaseViewModel.js
+++ src/BaseViewModel.js
@@ -4,28 +4,31 @@
     } else if (typeof module === "object" && module.exports) {
         module.exports = factory();
     } else {
-        root.BaseViewModel = factory();
+        root.Component = factory();
     }
 }(this, function() {
 
     var uniqueId = 0,
         bodyEl = document.getElementsByTagName("body")[0],
         headEl = document.getElementsByTagName("head")[0];
+
     BaseViewModel = function(options) {
+        var componentName = options.name || 'component-' + uniqueId++,
+
+            controller = options.controller;
+
+
         return function() {
 
+            var scope = this,
+                div = document.createElement('div');
+
+            // getting DOM
+            div.innerHTML = options.html || '';
+
+            var elements = scope.$elements = div.childNodes.length > 0 ? div.childNodes || div;
+
         };
-        var moduleName = options.moduleName || 'module' + uniqueId++,
-            controller = options.controller,
-            div = document.createElement('div'),
-            elements;
-
-        // getting DOM
-        div.innerHTML = options.html || '';
-        elements = controller.prototype.elements = div.childNodes.length > 0 ? div.childNodes || div;
-
-
-        return controller;
     };
 
     return BaseViewModel;
