8fd54b0b0cf5f3d35ced6795cb15ffd5b681da5f
Lalu Lavanya Busam <lalu@Lalus-MacBook-Air.local>
2016-09-13 14:32:19 +0530

    basic working tool is ready.

--- src/MakeComponent.js
+++ src/MakeComponent.js
@@ -10,9 +10,14 @@
 
     var componentNameCounter = 0;
     var componentIdCounter = 0;
-    var bodyEl = document.getElementsByTagName('body')[0];
+    //var bodyEl = document.getElementsByTagName('body')[0];
     var headEl = document.getElementsByTagName('head')[0];
+    var each = _.each;
 
+    var tempDiv = document.createElement('div');
+    var eventsList = ["abort", "beforecopy", "beforecut", "beforepaste", "blur", "cancel", "canplay", "canplaythrough", "change", "click", "close", "contextmenu", "copy", "cuechange", "cut", "dblclick", "drag", "dragend", "dragenter", "dragleave", "dragover", "dragstart", "drop", "durationchange", "emptied", "ended", "error", "focus", "input", "invalid", "keydown", "keypress", "keyup", "load", "loadeddata", "loadedmetadata", "loadstart", "mousedown", "mouseenter", "mouseleave", "mousemove", "mouseout", "mouseover", "mouseup", "mousewheel", "paste", "pause", "play", "playing", "progress", "ratechange", "reset", "resize", "scroll", "search", "seeked", "seeking", "select", "selectstart", "show", "stalled", "submit", "suspend", "timeupdate", "toggle", "volumechange", "waiting", "webkitfullscreenchange", "webkitfullscreenerror", "wheel"];
+
+    // addClass function from youmightnotneedjquery.com
     var addClass = function(el, className) {
         if (el.classList) {
             addClass = function(el, className) {
@@ -26,77 +31,151 @@
         addClass(el, className);
     };
 
-    var property = function(key) {
-        return function(obj) {
-            return obj == null ? void 0 : obj[key];
-        };
-    };
-    var getLength = property('length');
-    var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
-    var isArrayLike = function(collection) {
-        var length = getLength(collection);
-        return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
-    };
-    var each = function(obj, iteratee, context) {
-        var i, length;
-        if (isArrayLike(obj)) {
-            for (i = 0, length = obj.length; i < length; i++) {
-                iteratee(obj[i], i, obj);
-            }
-        } else {
-            var keys = _.keys(obj);
-            for (i = 0, length = keys.length; i < length; i++) {
-                iteratee(obj[keys[i]], keys[i], obj);
-            }
-        }
-        return obj;
-    };
-
     var MakeComponent = function(options) {
         var componentName = options.name ||
             'component-' + componentNameCounter++;
 
+        // personal stylesheet adding function
         var addStyleSheet = function() {
             if (options.css) {
                 var styleEl = document.createElement('style');
                 styleEl.innerHTML = options.css;
                 headEl.appendChild(styleEl);
             }
-            // making it work for only one time
+            // making it work for only one time per component
             addStyleSheet = function() {};
         }
 
-        var binders = {};
-        this.addBinder = function(name, callback) {
-            binders[name] = callback;
-        }
-
         return function() {
-            var that = this;
+
             addStyleSheet();
-            var component = new options.controller();
-            var div = document.createElement('div');
 
-            div.innerHTML = options.html || '<div></div>';
-            var elements = that.$elements = div.children;
+            var that = this,
+                properties = that.$properties = {};
 
-            var bound = [];
-            // finish this afterwards
-            each(that.$elements, function(element) {
-                addClass(element, componentName);
-                each(binders, function(callback, name) {
-                    var els = element.querySelectorAll('[' + name + ']');
-                    each(els, function(el) {
-                        var funcName = element.getAttribute(name);
-                        var func = component[funcName];
-                        // finish this afterwards
+            that.get = function(prop) {
+                return properties[prop];
+            };
+
+            that.set = function(prop, value) {
+                properties[prop] = value;
+                evaluatePropertyBindings(prop, value);
+            };
+
+            var propertyBindings = that.$propertyBindings = {};
+
+            // binding evaluator
+            var evaluatePropertyBindings = function(prop, value) {
+                if (prop) {
+                    var bindingsList = propertyBindings[prop];
+                    each(bindingsList, function(bindingCallback) {
+                        bindingCallback(value);
                     })
-                })
+                } else {
+                    // in case prop is not provided
+                    // evaluates everything
+                    each(propertyBindings, function(list, propName) {
+                        evaluatePropertyBindings(propName, that.get(propName));
+                    })
+                }
+            }
+
+            // setting elements
+            var div = document.createElement('div');
+            div.innerHTML = options.html || '<div></div>';
+
+            // just in case the internal elements are multiple
+            var elements = that.$elements = div.children;
+            each(elements, function(element) {
+
+                // adding this components personalized class
+                addClass(element, componentName);
+
+                // generates property bindings
+                MakeComponent.linkBinders(element, that);
+
+                // attaching known events
+                MakeComponent.attachEvents(element, that);
             });
 
+            // initializing the controller
+            var component = new options.controller(that);
+
+            // evaluate all bindings after controller initialization
+            evaluatePropertyBindings();
         }
     }
 
+
+    MakeComponent.linkBinders = function(element, that) {
+        each(MakeComponent.binders, function(callback, name) {
+            var els = element.querySelectorAll('[' + name + ']');
+            each(els, function(el) {
+                var prop = el.getAttribute(name);
+                var bindingsList = that.$propertyBindings[prop] || [];
+                bindingsList.push(callback(el, prop, that));
+                that.$propertyBindings[prop] = bindingsList;
+            })
+        });
+    };
+
+    MakeComponent.attachEvent = function(event, element, handler) {
+        if (element.addEventListener) { // DOM standard
+            attachEvent = function(event, element, handler) {
+                element.addEventListener(event, handler, false)
+            }
+        } else if (element.attachEvent) { // IE
+            attachEvent = function(event, element, handler) {
+                element.attachEvent('on' + event, handler);
+            }
+        }
+        attachEvent(event, element, handler);
+    }
+
+    MakeComponent.attachEvents = function(element, that) {
+        each(eventsList, function(name) {
+            var els = element.querySelectorAll('[' + name + '-handle]');
+            each(els, function(el) {
+                var prop = el.getAttribute(name + '-handle');
+                MakeComponent.attachEvent(name, el, function(event) {
+                    that[prop] && that[prop](event);
+                })
+            })
+        });
+    };
+
+    MakeComponent.binders = {};
+    MakeComponent.addBinder = function(name, callback) {
+        MakeComponent.binders[name] = callback;
+    };
+
+    MakeComponent.addBinder('bind-text', function(el, prop, that) {
+        return function(value) {
+            el.innerText = value;
+        }
+    });
+
+    MakeComponent.addBinder('bind-html', function(el, prop, that) {
+        return function(value) {
+            el.innerHTML = value;
+        }
+    });
+
+    MakeComponent.addBinder('bind-property', function(el, prop, that) {
+        var handler = function(event) {
+            that.set(prop, event.target.value);
+        }
+        MakeComponent.attachEvent('change', el, handler);
+        MakeComponent.attachEvent('keydown', el, handler);
+        MakeComponent.attachEvent('paste', el, handler);
+        MakeComponent.attachEvent('blur', el, handler);
+        MakeComponent.attachEvent('focus', el, handler);
+        return function(value) {
+            if (el.value) {
+                el.value = value;
+            }
+        }
+    });
 
     return MakeComponent;
 }));
